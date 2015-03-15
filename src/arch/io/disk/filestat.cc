dbc9cf42e4c045e80b07d634003c4b89a235c6fe
Jeffrey Drake <jptdrake@gmail.com>
2015-03-15 04:29:04 -0400

    Filestat operating system dependencies updated for OpenBSD

--- src/arch/io/disk/filestat.cc
+++ src/arch/io/disk/filestat.cc
@@ -7,12 +7,14 @@
 int64_t get_file_size(int fd) {
     guarantee(fd != -1);
 
-#ifdef __MACH__
+#if defined(__MACH__) || defined(__OpenBSD__)
     struct stat buf;
     int res = fstat(fd, &buf);
-#else
+#elif defined(__linux__)
     struct stat64 buf;
     int res = fstat64(fd, &buf);
+#else
+    #error Insert your operating system fstat function here.
 #endif
     // This compile-time assertion is the most important line in the function.
     CT_ASSERT(sizeof(buf.st_size) == sizeof(int64_t));
